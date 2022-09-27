fee2574f254a842715e988c2dd47148486deac46
Alexey Makeev <makeevhome@mail.ru>
2022-09-28 01:28:26 +0300

    add code

--- GBcontrol/Program.cs
+++ GBcontrol/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class MyClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите несколько слов через пробел: ");
+        var str = Console.ReadLine().Split(" ");
+        var str2 = String.Join("", str).ToCharArray();
+
+        string[] str3 = new string[str2.Length / 3 + 1];
+        for (int i = 0; i < str2.Length; i++ )
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                str3[i] = "" + str2[j];
+            }
+            
+        }
+
+
+        Console.WriteLine(String.Join(", ", str3));
+    }
+}
